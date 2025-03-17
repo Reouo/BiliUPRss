776e4dbdba6b1e7d69cf15d2a7b53832048166bb
yangfan <MagicSakuraLibrary@hotmail.com>
2025-03-17 21:19:07 +0800

    V0.1.0  --  完成了数据库存储 计划: 写筛选函数 (缺点: 大部分操作在内存完成，可能可以进一步优化)

--- bili_requests_functions.py
+++ bili_requests_functions.py
@@ -1,15 +1,17 @@
 import os
+import re
+import time
+from datetime import datetime, timedelta
+
+import psycopg2
+import pytz
 import requests
 from fake_useragent import UserAgent
 from feedgen.entry import FeedEntry
 from feedgen.feed import FeedGenerator
-from datetime import datetime, timedelta
-import pytz
-import re
-import time
 
 
-# 日期转化
+# 日期转化(适用于rss)
 def parse_and_format_date(date_str=None):
     if date_str:
         # 处理不同格式的日期字符串
@@ -38,6 +40,15 @@ def parse_and_format_date(date_str=None):
             # 格式: "n天前"
             days = int(re.search(r'\d+', date_str).group())
             dt = datetime.now(pytz.timezone('Asia/Shanghai')) - timedelta(days=days)
+        elif re.match(r'^昨天\s+\d{2}:\d{2}$', date_str):
+            # 格式: "昨天 20:34"
+            time_part = re.search(r'\d{2}:\d{2}', date_str).group()
+            current_year = datetime.now(pytz.timezone('Asia/Shanghai')).year
+            current_month = datetime.now(pytz.timezone('Asia/Shanghai')).month
+            current_day = datetime.now(pytz.timezone('Asia/Shanghai')).day
+            yesterday = datetime(current_year, current_month, current_day, 0, 0, 0, tzinfo=pytz.timezone('Asia/Shanghai')) - timedelta(days=1)
+            input_format = '%Y-%m-%d %H:%M'
+            dt = datetime.strptime(f"{yesterday.strftime('%Y-%m-%d')} {time_part}", input_format)
         else:
             raise ValueError(f"无法解析的日期格式: {date_str}")
     else:
@@ -49,7 +60,21 @@ def parse_and_format_date(date_str=None):
     return dt_utc.strftime('%a, %d %b %Y %H:%M:%S %z')
 
 
-# 拿到图片后缀
+# 日期转化(在parse_and_format_date基础上，适用于sql里的date)
+def load_and_format_date(strf_time):
+    # 解析日期字符串为 datetime 对象
+    try:
+        # 尝试解析为 '%a, %d %b %Y %H:%M:%S %z' 格式
+        dt = datetime.strptime(strf_time, '%a, %d %b %Y %H:%M:%S %z')
+    except ValueError:
+        # 如果解析失败，尝试解析为其他格式
+        dt = parse_and_format_date(strf_time)
+
+    # 转换为 PostgreSQL 的 DATE 格式
+    return dt.date()
+
+
+# 拿到图片后缀，确定附件的解析方式(可能多余)
 def get_mime_type(url):
     # 分析图片后缀
     _, ext = os.path.splitext(url)
@@ -64,7 +89,8 @@ def get_mime_type(url):
     return mime_types.get(ext.lower(), 'application/octet-stream')
 
 
-def get_id_title_time_text_pics_list(up_uid, user_cookie):
+# 获取如函数名所示的各项数据的字典的列表
+def get_name_id_title_time_text_pics_list(up_uid, user_cookie):
     # features可能因人而异
     features = 'itemOpusStyle,listOnlyfans,opusBigCover,onlyfansVote,forwardListHidden,decorationCard,commentsNewVersion,onlyfansAssetsV2,ugcDelete,onlyfansQaCard'
     origin_url = 'https://space.bilibili.com'
@@ -82,8 +108,8 @@ def get_id_title_time_text_pics_list(up_uid, user_cookie):
         'Cookie': user_cookie}
     response = requests.get(space_items_url, headers=headers)
     data = response.json()
-
-    id_title_time_text_pics_type_list = []
+    up_name = data['data']['items'][0]['modules']['module_author']['name']
+    name_id_title_time_text_pics_type_list = []
     for item in data['data']['items']:
         data_type = item['type']
         # 处理图文以及纯文本
@@ -93,9 +119,11 @@ def get_id_title_time_text_pics_list(up_uid, user_cookie):
             data_time = item['modules']['module_author']['pub_time']
             data_text = item['modules']['module_dynamic']['major']['opus']['summary']['text']
             data_pics = item['modules']['module_dynamic']['major']['opus']['pics']
-            id_title_time_text_pics_type_list.append(
-                {'id': data_id, 'title': data_title, 'time': data_time, 'text': data_text, 'pics': data_pics,
+            name_id_title_time_text_pics_type_list.append(
+                {'name': up_name, 'id': data_id, 'title': data_title, 'time': data_time, 'text': data_text,
+                 'pics': data_pics,
                  'type': data_type})
+            print('获取了一条图文动态')
         # 处理视频
         elif data_type == 'DYNAMIC_TYPE_AV':
             data_title = item['modules']['module_dynamic']['major']['archive']['title']
@@ -103,9 +131,11 @@ def get_id_title_time_text_pics_list(up_uid, user_cookie):
             data_bvid = item['modules']['module_dynamic']['major']['archive']['bvid']
             data_desc = item['modules']['module_dynamic']['major']['archive']['desc']
             data_pic = item['modules']['module_dynamic']['major']['archive']['cover']
-            id_title_time_text_pics_type_list.append(
-                {'title': data_title, 'time': data_time, 'bvid': data_bvid, 'desc': data_desc, 'pic': data_pic,
+            name_id_title_time_text_pics_type_list.append(
+                {'name': up_name, 'title': data_title, 'time': data_time, 'bvid': data_bvid, 'desc': data_desc,
+                 'pic': data_pic,
                  'type': data_type})
+            print('获取了一条视频动态')
         # 处理专栏
         elif data_type == 'DYNAMIC_TYPE_ARTICLE':
             data_id = item['basic']['rid_str']
@@ -124,8 +154,6 @@ def get_id_title_time_text_pics_list(up_uid, user_cookie):
             content = response.json()
             content_title = content['data']['title']
             content_time = content['data']['publish_time']
-            print(content_time)
-            print(type(content_time))
             # 当专栏中有图片时
             if 'opus' in content['data']:
                 content_text = ''
@@ -136,95 +164,97 @@ def get_id_title_time_text_pics_list(up_uid, user_cookie):
                     if item['para_type'] == 2:
                         content_pics.append(item['pic']['pics'][0]['url'])
 
-                id_title_time_text_pics_type_list.append(
-                    {'id': data_id, 'title': content_title, 'time': content_time, 'text': content_text,
+                name_id_title_time_text_pics_type_list.append(
+                    {'name': up_name, 'id': data_id, 'title': content_title, 'time': content_time, 'text': content_text,
                      'pics': content_pics, 'type': data_type})  # 这里的pics只需要按顺序取出就行了，里面是字符串不是字典
             # 纯文本专栏
             else:
                 content_text = content['data']['content']
                 content_pics = []
-                id_title_time_text_pics_type_list.append(
-                    {'id': data_id, 'title': content_title, 'time': content_time, 'text': content_text,
-                     'type': data_type})
+                name_id_title_time_text_pics_type_list.append(
+                    {'name': up_name, 'id': data_id, 'title': content_title, 'time': content_time, 'text': content_text,
+                     'pics': content_pics, 'type': data_type})
             time.sleep(15)
-    return id_title_time_text_pics_type_list
+            print('获取了一个专栏动态\nsleeping···')
+    return name_id_title_time_text_pics_type_list
 
 
-def load_rss(id_title_time_text_pics_list, up_uid):
+# 生成rss
+def load_rss(name_id_title_time_text_pics_list, up_uid):
     # 初始化 RSS 生成器
     fg = FeedGenerator()
     fg.id(f'https://space.bilibili.com/{up_uid}')
-    fg.title(f'{up_uid}的B站动态')
+    fg.title(f'{name_id_title_time_text_pics_list[0]['name']}的B站动态')
     fg.link(href=f'https://space.bilibili.com/{up_uid}')
-    fg.description(f'{up_uid}的B站动态')
+    fg.description(f'{name_id_title_time_text_pics_list[0]['name']}的B站动态')
     fg.lastBuildDate(parse_and_format_date())
 
     # 遍历动态数据
-    for id_title_time_text_pics_dict in id_title_time_text_pics_list:
+    for item in name_id_title_time_text_pics_list:
         # 图文或纯文本动态
-        if id_title_time_text_pics_dict['type'] == 'DYNAMIC_TYPE_DRAW' or id_title_time_text_pics_dict[
+        if item['type'] == 'DYNAMIC_TYPE_DRAW' or item[
             'type'] == 'DYNAMIC_TYPE_WORD':
             entry = FeedEntry()
-            entry.id(f"https://www.bilibili.com/opus/{id_title_time_text_pics_dict['id']}")
-            entry.title(id_title_time_text_pics_dict['title'])
-            entry.link(href=f"https://www.bilibili.com/opus/{id_title_time_text_pics_dict['id']}")
+            entry.id(f"https://www.bilibili.com/opus/{item['id']}")
+            entry.title(item['title'])
+            entry.link(href=f"https://www.bilibili.com/opus/{item['id']}")
 
             # 在 description 中嵌入图片
-            description = id_title_time_text_pics_dict['text']
-            for pic in id_title_time_text_pics_dict['pics']:
+            description = item['text']
+            for pic in item['pics']:
                 description += f'<br><img src="{pic["url"]}">'
             entry.description(description)
 
             # 添加图片附件（保留 enclosure）
-            for pic in id_title_time_text_pics_dict['pics']:
+            for pic in item['pics']:
                 entry.enclosure(pic['url'], 0, get_mime_type(pic['url']))
 
             # 设置发布时间
-            entry.pubDate(parse_and_format_date(id_title_time_text_pics_dict['time']))
+            entry.pubDate(parse_and_format_date(item['time']))
 
             # 添加到 RSS
             fg.add_entry(entry)
         # 视频动态
-        elif id_title_time_text_pics_dict['type'] == 'DYNAMIC_TYPE_AV':
+        elif item['type'] == 'DYNAMIC_TYPE_AV':
             entry = FeedEntry()
-            entry.id(f"https://www.bilibili.com/video/{id_title_time_text_pics_dict['bvid']}")
-            entry.title(id_title_time_text_pics_dict['title'])
-            entry.link(href=f"https://www.bilibili.com/video/{id_title_time_text_pics_dict['bvid']}")
+            entry.id(f"https://www.bilibili.com/video/{item['bvid']}")
+            entry.title(item['title'])
+            entry.link(href=f"https://www.bilibili.com/video/{item['bvid']}")
 
             # 使用 iframe 嵌入视频播放器
-            player_url = f"https://player.bilibili.com/player.html?aid=&bvid={id_title_time_text_pics_dict['bvid']}&cid=&p=1&as_wide=1&high_quality=1&danmaku=0&t=0"
-            description = id_title_time_text_pics_dict['desc']
+            player_url = f"https://player.bilibili.com/player.html?aid=&bvid={item['bvid']}&cid=&p=1&as_wide=1&high_quality=1&danmaku=0&t=0"
+            description = item['desc']
             description += f'<br><iframe width="560" height="315" src="{player_url}" frameborder="0" allowfullscreen></iframe>'
 
             # 获取封面图片链接
-            cover_image_url = id_title_time_text_pics_dict['pic']
+            cover_image_url = item['pic']
             description += f'<br><img src="{cover_image_url}">'
 
             entry.description(description)
 
-            entry.pubDate(parse_and_format_date(id_title_time_text_pics_dict['time']))
+            entry.pubDate(parse_and_format_date(item['time']))
             fg.add_entry(entry)
 
 
         # 专栏动态
-        elif id_title_time_text_pics_dict['type'] == 'DYNAMIC_TYPE_ARTICLE':
+        elif item['type'] == 'DYNAMIC_TYPE_ARTICLE':
             entry = FeedEntry()
-            entry.id(f"https://www.bilibili.com/read/cv{id_title_time_text_pics_dict['id']}")
-            entry.title(id_title_time_text_pics_dict['title'])
-            entry.link(href=f"https://www.bilibili.com/read/cv{id_title_time_text_pics_dict['id']}")
+            entry.id(f"https://www.bilibili.com/read/cv{item['id']}")
+            entry.title(item['title'])
+            entry.link(href=f"https://www.bilibili.com/read/cv{item['id']}")
 
             # 在 description 中嵌入图片
-            description = id_title_time_text_pics_dict['text']
-            for pic in id_title_time_text_pics_dict['pics']:
+            description = item['text']
+            for pic in item['pics']:
                 description += f'<br><img src="{pic}">'
             entry.description(description)
 
             # 添加图片附件（保留 enclosure）
-            for pic in id_title_time_text_pics_dict['pics']:
+            for pic in item['pics']:
                 entry.enclosure(pic, 0, get_mime_type(pic))
 
             # 设置发布时间
-            entry.pubDate(parse_and_format_date(id_title_time_text_pics_dict['time']))
+            entry.pubDate(parse_and_format_date(item['time']))
 
             # 添加到 RSS
             fg.add_entry(entry)
@@ -237,4 +267,91 @@ def load_rss(id_title_time_text_pics_list, up_uid):
     # 写入 RSS 到文件
     rss_output_path = os.path.join(output_dir, f'{up_uid}.xml')
     fg.rss_file(rss_output_path, pretty=True)  # 使用 rss_file 方法
-    print(f'RSS 文件已输出到 {rss_output_path}')
+    print(f'RSS文件已输出到 {rss_output_path}')
+
+
+# 建表
+def create_bili_dynamics_table():
+    connect = psycopg2.connect(database='reouo', user='postgres', password='12345', host='127.0.0.1', port='5432')
+    cursor = connect.cursor()
+    sql = '''CREATE TABLE IF NOT EXISTS bili_dynamics (
+        up_name CHARACTER VARYING, 
+        detail_url CHARACTER VARYING PRIMARY KEY,
+        title TEXT,
+        time DATE,
+        text TEXT,
+        pics TEXT[],
+        type CHARACTER VARYING
+    );'''
+    cursor.execute(sql)
+    connect.commit()
+    cursor.close()
+    connect.close()
+    print('Table创建成功/已存在')
+
+
+# 写数据
+def write_bili_dynamics_table(name_id_title_time_text_pics_type_list):
+    connect = psycopg2.connect(database='reouo', user='postgres', password='12345', host='127.0.0.1', port='5432')
+    cursor = connect.cursor()
+    for item in name_id_title_time_text_pics_type_list:
+        if item['type'] == 'DYNAMIC_TYPE_DRAW' or item['type'] == 'DYNAMIC_TYPE_WORD':
+            up_name = item['name']
+            detail_url = f'https://www.bilibili.com/opus/{item['id']}'
+            title = item['title']
+            time = load_and_format_date(parse_and_format_date(item['time']))
+            text = item['text']
+            pics = []
+            for pic in item['pics']:
+                pics.append(pic['url'])
+            type = item['type']
+            insert_sql = '''
+            INSERT INTO bili_dynamics (up_name, detail_url, title, time, text, pics, type)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (detail_url) DO NOTHING;
+            '''
+            cursor.execute(insert_sql, (up_name, detail_url, title, time, text, pics, type))
+        elif item['type'] == 'DYNAMIC_TYPE_AV':
+            up_name = item['name']
+            detail_url = f'https://www.bilibili.com/video/{item['bvid']}'
+            title = item['title']
+            time = load_and_format_date(parse_and_format_date(item['time']))
+            text = item['desc']
+            pics = []
+            pics.append(item['pic'])
+            type = item['type']
+            insert_sql = '''
+            INSERT INTO bili_dynamics (up_name, detail_url, title, time, text, pics, type)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (detail_url) DO NOTHING;
+            '''
+            cursor.execute(insert_sql, (up_name, detail_url, title, time, text, pics, type))
+        elif item['type'] == 'DYNAMIC_TYPE_ARTICLE':
+            up_name = item['name']
+            detail_url = f'https://www.bilibili.com/read/cv{item['id']}'
+            title = item['title']
+            time = load_and_format_date(parse_and_format_date(item['time']))
+            text = item['text']
+            pics = []
+            for pic in item['pics']:
+                pics.append(pic)
+            type = item['type']
+            insert_sql = '''
+            INSERT INTO bili_dynamics (up_name, detail_url, title, time, text, pics, type)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (detail_url) DO NOTHING;
+            '''
+            cursor.execute(insert_sql, (up_name, detail_url, title, time, text, pics, type))
+    connect.commit()
+    cursor.close()
+    connect.close()
+    print('数据成功写入数据库')
+def filter_contents():
+    pass
+
+if __name__ == '__main__':
+    up_uid = '145239325'
+    user_cookie = "buvid3=285ADB51-97F2-B63C-7F72-D16A756FBE3E16395infoc; b_nut=1740126116; _uuid=55CDAAA2-3CEE-A666-7942-7A85BB610EE6D23000infoc; enable_web_push=DISABLE; enable_feed_channel=ENABLE; home_feed_column=5; browser_resolution=1655-979; buvid4=EE80E339-C0FA-5EBE-A5F7-570E11CB4B0116972-025022108-Azs0hNockNm%2BtYmGHF5HMg%3D%3D; buvid_fp=0e6ee00f0d9ee5987c15973ffb9cce04; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDIwNDg3MjcsImlhdCI6MTc0MTc4OTQ2NywicGx0IjotMX0.NMHnq6PWgJ713_9zHeboy5nDKO7xDoC17UzdLNlX3cs; bili_ticket_expires=1742048667; header_theme_version=CLOSE; bp_t_offset_531140325=1043777155730767872; CURRENT_FNVAL=16; rpdid=|(JY~|J)|J~R0J'u~R|umYlYY; fingerprint=0e6ee00f0d9ee5987c15973ffb9cce04; buvid_fp_plain=undefined; CURRENT_QUALITY=120; PVID=3; LIVE_BUVID=AUTO5817406608563424; hit-dyn-v2=1; SESSDATA=c89efa97%2C1757327186%2C2aa96%2A32CjCpWs8aFtXARSQCCLhT_jaMFZxXZo1IrI1RIab2xqm5h6sEZcSeaCPIP-PwZzwelvESVjBhVGtNRzJEaVB0OEl0ekNLMVNwT3EwaDQyZ3prM0dCcjE2OVR3WURVRTB1SzZhd242eUhQemNjQTNxbXUweE9iZTF3cWJqdEEwdlA1RGdqeTdnS3FRIIEC; bili_jct=65215689e8b17793cafe3f80ddb23362; DedeUserID=531140325; DedeUserID__ckMd5=9480e1630b93a4f3; opus-goback=1; b_lsid=102BB994D_1958F5E0CF3; sid=4lofzu1i"
+    # 以上传参
+    name_id_title_time_text_pics_type_list = get_name_id_title_time_text_pics_list(up_uid, user_cookie)
+    write_bili_dynamics_table(name_id_title_time_text_pics_type_list)
--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from bili_requests_functions import *
 import os
 
 if __name__ == '__main__':
-    up_uid = os.environ.get('UP_UID')
-    user_cookie = os.environ.get('USER_COOKIE')
+    # up_uid = os.environ.get('UP_UID')
+    # user_cookie = os.environ.get('USER_COOKIE')
+    #
+    # if not up_uid:
+    #     raise ValueError("UP_UID 环境变量未设置")
+    # if not user_cookie:
+    #     raise ValueError("USER_COOKIE 环境变量未设置")
 
-    if not up_uid:
-        raise ValueError("UP_UID 环境变量未设置")
-    if not user_cookie:
-        raise ValueError("USER_COOKIE 环境变量未设置")
-
+    up_uid='108919422'
+    user_cookie="buvid3=285ADB51-97F2-B63C-7F72-D16A756FBE3E16395infoc; b_nut=1740126116; _uuid=55CDAAA2-3CEE-A666-7942-7A85BB610EE6D23000infoc; enable_web_push=DISABLE; enable_feed_channel=ENABLE; home_feed_column=5; browser_resolution=1655-979; buvid4=EE80E339-C0FA-5EBE-A5F7-570E11CB4B0116972-025022108-Azs0hNockNm%2BtYmGHF5HMg%3D%3D; buvid_fp=0e6ee00f0d9ee5987c15973ffb9cce04; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDIwNDg3MjcsImlhdCI6MTc0MTc4OTQ2NywicGx0IjotMX0.NMHnq6PWgJ713_9zHeboy5nDKO7xDoC17UzdLNlX3cs; bili_ticket_expires=1742048667; header_theme_version=CLOSE; bp_t_offset_531140325=1043777155730767872; CURRENT_FNVAL=16; rpdid=|(JY~|J)|J~R0J'u~R|umYlYY; fingerprint=0e6ee00f0d9ee5987c15973ffb9cce04; buvid_fp_plain=undefined; CURRENT_QUALITY=120; PVID=3; LIVE_BUVID=AUTO5817406608563424; hit-dyn-v2=1; SESSDATA=c89efa97%2C1757327186%2C2aa96%2A32CjCpWs8aFtXARSQCCLhT_jaMFZxXZo1IrI1RIab2xqm5h6sEZcSeaCPIP-PwZzwelvESVjBhVGtNRzJEaVB0OEl0ekNLMVNwT3EwaDQyZ3prM0dCcjE2OVR3WURVRTB1SzZhd242eUhQemNjQTNxbXUweE9iZTF3cWJqdEEwdlA1RGdqeTdnS3FRIIEC; bili_jct=65215689e8b17793cafe3f80ddb23362; DedeUserID=531140325; DedeUserID__ckMd5=9480e1630b93a4f3; opus-goback=1; b_lsid=102BB994D_1958F5E0CF3; sid=4lofzu1i"
     # 以上传参
     id_title_time_text_pics_list = get_id_title_time_text_pics_list(up_uid, user_cookie)
     load_rss(id_title_time_text_pics_list, up_uid)
